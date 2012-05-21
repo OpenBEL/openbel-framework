0919b92d44d0ffd4b7b362c2733c654bc9eba24f
Anthony Bargnesi <abargnesi@gmail.com>
2012-05-21 09:46:18 -0400

    fixed package script for shells running dash (i.e ubuntu)
    - added read "NULL" argument to wait for user input before execution

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 VERSION=$(grep "^    <version>.*</version>" pom.xml -o \
     | grep ">.*<" -o | tr -d '<>')
-echo -n "Ready to package ${VERSION}. Hit [ENTER] or [CTRL-C]."; read
+echo -n "Ready to package ${VERSION}. Hit [ENTER] or [CTRL-C]."; read NULL
 mvn -Pdistribution clean package assembly:assembly install
 
