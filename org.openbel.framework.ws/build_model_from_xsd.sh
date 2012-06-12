db13eef42edbc246e5869c82d619178af2eaf7a6
jmcmahon <jmcmahon@selventa.com>
2012-06-12 09:45:56 -0400

    Removed duplicate .xsd files from resources
    There is now one canonical set of .xsd files in the WEB-INF/xsd
    directory.

--- org.openbel.framework.ws/build_model_from_xsd.sh
+++ org.openbel.framework.ws/build_model_from_xsd.sh
@@ -2,8 +2,5 @@
 xjc -p org.openbel.framework.ws.model \
     -d ./src/main/java/ \
     -no-header \
-    ./src/main/resources/belframework-web-api.xsd 
-
-# copy the scehma to the WEB-INF/xsd folder
-cp ./src/main/resources/*.xsd ./src/main/webapp/WEB-INF/xsd
+    ./src/main/webapp/WEB-INF/xsd/belframework-web-api.xsd 
 
