70b3ccea09da3ae50ad6ea351f01b9b709f968d5
Anthony Bargnesi <abargnesi@gmail.com>
2012-06-27 12:21:53 -0400

    cleaned up simple warnings
    - unused field in KamBuilder test helper
    - superfluous semicolon in KamComparator

--- org.openbel.framework.api/src/test/java/org/openbel/framework/api/KamBuilder.java
+++ org.openbel.framework.api/src/test/java/org/openbel/framework/api/KamBuilder.java
@@ -17,15 +17,12 @@ import org.openbel.framework.internal.KAMCatalogDao.KamInfo;
 
 public class KamBuilder {
     private KamInfo kamInfo;
-    private final KamStore kamStore;
     private final Map<String, TestKamNode> knodes;
     private final Map<String, TestKamEdge> kedges;
     private final boolean usingLongForm;
-    
-    public KamBuilder(final KamInfo kamInfo, final KamStore kamStore,
-            final boolean usingLongForm) {
+
+    public KamBuilder(final KamInfo kamInfo, final boolean usingLongForm) {
         this.kamInfo = kamInfo;
-        this.kamStore = kamStore;
         this.knodes = new LinkedHashMap<String, TestKamNode>();
         this.kedges = new LinkedHashMap<String, TestKamEdge>();
         this.usingLongForm = usingLongForm;
@@ -40,10 +37,10 @@ public class KamBuilder {
                         .getFunctionEnum(), bel));
             }
         }
-        
+
         return this;
     }
-   
+
     public KamBuilder addEdges(final Edge... edges) throws RecognitionException {
         if (hasItems(edges)) {
             for (final Edge e : edges) {
@@ -55,16 +52,16 @@ public class KamBuilder {
                 if (objectNode == null) {
                     throw new IllegalStateException("Object node does not exist, error on: " + e);
                 }
-                
+
                 String bel = e.subject + " " + e.rel + " " + e.object;
                 kedges.put(bel, new TestKamEdge(kedges.size(), subjectNode,
                         e.rel, objectNode));
             }
         }
-        
+
         return this;
     }
-    
+
     public Kam create() {
         Collection<TestKamNode> nodes = knodes.values();
         Collection<TestKamEdge> edges = kedges.values();
@@ -72,27 +69,27 @@ public class KamBuilder {
                 nodes.toArray(new TestKamNode[nodes.size()]),
                 edges.toArray(new TestKamEdge[edges.size()]));
     }
-    
+
     public KamBuilder clear() {
         knodes.clear();
         kedges.clear();
-        
+
         return this;
     }
 
     public static Edge edge(final String subject, final RelationshipType rel, final String object) {
         return new Edge(subject, rel, object);
     }
-    
+
     private String getBEL(final BELObject o) {
         return usingLongForm ? o.toBELLongForm() : o.toBELShortForm();
     }
-    
+
     public static final class Edge {
         private final String subject;
         private final RelationshipType rel;
         private final String object;
-        
+
         public Edge(String subject, RelationshipType rel, String object) {
             this.subject = subject;
             this.rel = rel;
