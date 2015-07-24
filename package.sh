0d7a79a5e51bebc121cd06b41ccf94f178f02c5e
Nick Bargnesi <nbargnesi@selventa.com>
2015-07-24 13:54:02 -0400

    use portal interpreter directive (usr/bin/env)

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 VERSION=$(grep "^    <version>.*</version>" pom.xml -o \
     | grep ">.*<" -o | tr -d '<>')
 echo -n "Ready to package ${VERSION}. Hit [ENTER] or [CTRL-C]."; read NULL
